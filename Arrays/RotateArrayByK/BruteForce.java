5e2e3dddf17afc2aca01a516c0e8e3d223a22074
Dikshi Sriram <178579382+Dikshithaa556@users.noreply.github.com>
2026-06-27 15:39:12 +0530

    Added Reversal algorithm for clockwise rotation and corrected some code

--- Arrays/RotateArrayByK/BruteForce.java
+++ Arrays/RotateArrayByK/BruteForce.java
@@ -1,13 +1,27 @@
 package Arrays.RotateArrayByK;
 
 public class BruteForce {
+    //Anticlocķwise rotation
     public void rotateByK(int[] arr, int k){
+        int n =arr.length;
         for(int i=0; i<k; i++){
             int first = arr[0];
-            for(int j=0; j<arr.length-2; i++){
-                arr[i] = arr[i+1];
+            for(int j=0; j<arr.length-1; j++){
+                arr[j] = arr[j+1];
             }
-            arr[arr.length-1] = first;
+            arr[n-1] = first;
         }
-    }   
+    }  
+
+    //Clockwise rotation
+    public void rotateByKRight(int[] arr, int k){
+        int n = arr.length;
+        for(int i=0; i<k; i++){
+            int last = arr[n-1];
+            for(int j=n-2; j>=0; j--){
+                arr[j+1] = arr[j];
+            }
+            arr[0] = last;
+        }
+    }
 }
--- Arrays/RotateArrayByOne.java
+++ Arrays/RotateArrayByOne.java
@@ -1,13 +1,24 @@
 package Arrays;
 
 public class RotateArrayByOne {
-    
+    //Clockwise rotation
     public void rotate(int[] arr) {
-        int last = arr[arr.length-1];
-        for(int i=arr.length-1; i>=1; i--){
+        int n = arr.length;
+        int last = arr[n-1];
+        for(int i=n-1; i>0; i--){
             arr[i] = arr[i-1];
         }
          arr[0] = last;
+    }
+
+    //Anticlockwise rotation
+    public void rotateLeft(int[] arr) {
+        int n = arr.length;
+        int first = arr[0];
+        for(int i=1; i<n; i++){
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = first;
     }
     
 }
