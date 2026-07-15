726fdc7b4ea911e38182e608092cfec2e9a4268f
Dikshi Sriram <178579382+Dikshithaa556@users.noreply.github.com>
2026-07-15 06:30:51 +0530

    Added ArrayLeaders.java

---
+++ Arrays/Solution.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution{
+    static ArrayList<Integer> arrayLeaders(int[]arr){
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = arr.length;
+        int max = arr[n-1];
+        list.add(max);
+        for(int i=n-2; i>=0; i--){
+            if(arr[i]>=max){
+                max = arr[i];
+                list.add(max);
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
